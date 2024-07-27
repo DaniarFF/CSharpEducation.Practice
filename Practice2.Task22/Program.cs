ad3d76ad588d293ee9e6b4749e1710dc21a7c4ca
Dan <monreal007@gmail.com>
2024-07-27 16:44:56 +0400

    Практика 2. Задание 22

---
+++ Practice2.Task22/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice2.Task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год!");
+            int.TryParse(Console.ReadLine(), out int year);
+
+            if (year % 400 == 0 || year % 4 == 0) 
+            { 
+                Console.WriteLine("Год високосный"); 
+            }
+            if (year % 100 != 0)
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else { Console.WriteLine("Год невисокосный"); }
+        }
+    }
+}
+
