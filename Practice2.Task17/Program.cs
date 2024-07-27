b6c0dc2bbdd06eb81d122ab0c80cfcabff6112a7
Dan <monreal007@gmail.com>
2024-07-27 16:43:53 +0400

    Практика 2. Задание 17

---
+++ Practice2.Task17/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice2.Task17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 4;
+            Switch(ref a, ref b);
+
+            Console.WriteLine($"{a}, {b}");
+        }
+
+        static void Switch(ref int a, ref int b) 
+        {
+            int num = a;
+            a = b;
+            b = num;
+        }
+    }
+}
+
