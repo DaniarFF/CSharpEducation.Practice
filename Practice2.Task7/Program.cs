4c2e639b1db38f18dc613b13c8bce2883fdff89f
Dan <monreal007@gmail.com>
2024-07-27 16:41:24 +0400

    Практика 2. Задание 7

---
+++ Practice2.Task7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice2.Task7
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Practice 2. Task7
+        /// </summary>
+        /// <param name="args"></param>    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string line = Console.ReadLine();
+
+            TaskA(line);
+
+            TaskB(line);
+
+            TaskC(line);
+        }
+
+        static void TaskA(string line)
+        {
+            Console.WriteLine(line.ToUpper());
+        }
+
+        static void TaskB(string line)
+        {
+            Console.WriteLine(line.ToLower());
+        }
+
+        static void TaskC(string line)
+        {
+            line = line.ToLower();
+            string result = line.Substring(0, 1).ToUpper() + line.Substring(1);
+            Console.WriteLine(result);
+        }
+    }
+}
