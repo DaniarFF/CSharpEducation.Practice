96a4f72543b9d027973ac56ccc38e4b5a73ede1b
Dan <monreal007@gmail.com>
2024-08-03 11:27:06 +0400

    Практика 3. Задание 5

---
+++ Practice3.Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+using System;
+
+namespace Practice3.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Book
+    {
+        public string name;
+        public string author;
+        public DateTime Date { get; set; }
+
+        public Book(string name) : this(name, "Неизвестно")
+        {}
+        public Book(string name, string author)
+        {
+            this.name = name;
+            this.author = author;
+        }
+        public Book(string name, string author, DateTime date) : this(name, author)
+        {
+            Date = date;
+        }
+        public Book() { }
+    }
+}
+
+
