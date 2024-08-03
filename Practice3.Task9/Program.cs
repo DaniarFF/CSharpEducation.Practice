36bc08afd023fe91d3fe134bb5447920c15dda7c
Dan <monreal007@gmail.com>
2024-08-03 11:28:01 +0400

    Практика 3. Задание 9

---
+++ Practice3.Task9/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public class Student
+        {
+            public string name;
+            public int age;
+            public void MakeStudentAnonymous(Student student)
+            {
+                student.name = "Аноним";
+            }
+        }
+    }
+}
