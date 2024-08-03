90140b21b2af9a07fcaa7869220056f47dc036da
Dan <monreal007@gmail.com>
2024-08-03 11:28:34 +0400

    Практика 3. Задание 11

---
+++ Practice3.Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice3.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class Book
+        {
+            public string name;
+            public string author;
+            public string GetName(Book book) 
+            { 
+                return name;
+            }
+        }
+    }
+}
