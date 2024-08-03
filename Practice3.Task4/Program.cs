4ab5767cb01fb4402c640d6f74148ce0ba73f90f
Dan <monreal007@gmail.com>
2024-08-03 11:26:41 +0400

    Практика 3. Задание 4

---
+++ Practice3.Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice3.Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Book
+    {
+        public string name;
+        public string author;
+        public Book(string name, string author) 
+        { 
+            this.name = name;
+            this.author = author;
+        }
+        public Book() { }
+    }
+}
