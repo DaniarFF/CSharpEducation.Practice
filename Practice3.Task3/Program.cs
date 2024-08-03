ec3feb4efcdb7901a50b4c10b9e3ab5776d66a89
Dan <monreal007@gmail.com>
2024-08-03 11:26:24 +0400

    Практика 3. Задание 3

---
+++ Practice3.Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practice3.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Calculator 
+    { 
+        public double Sum(double x, double y) 
+        { 
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
