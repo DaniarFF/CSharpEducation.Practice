62087ad4e6acd87c8b5cc299ce23e30a7cfe3546
Dan <monreal007@gmail.com>
2024-08-03 11:27:45 +0400

    Практика 3. Задание 8

---
+++ Practice3.Task8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public struct Rectangle
+    {
+        public double width, height;
+
+        public double GetSquare(Rectangle rectangle) 
+        { 
+            return width * height;
+        }
+    }
+}
