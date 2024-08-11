8962d66eff02070336005013d67a4d39c69f3f5b
Dan <monreal007@gmail.com>
2024-08-11 10:50:13 +0400

    Практика 4. Задание 1-3

---
+++ Practice4.Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using MathHelperLibrary;
+using PersonLibrary;
+
+namespace Practice4.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2
+            int x = 5;
+            int y = 10;
+            MathHelper mathHelper = new MathHelper();
+
+            mathHelper.Div(x, y);
+            mathHelper.Sum(x, y);
+            mathHelper.Sub(x, y);
+            mathHelper.Multiply(x, y);
+            
+            //Task 3 
+
+            Person jeff = new Person() 
+            { 
+                name = "Jeff",
+                age = 35,
+            };
+
+            Person ivan = new Person()
+            {
+                name = "Ivan",
+                age = 14
+            };
+
+            jeff.GetPerson();
+            ivan.GetPerson();
+        }
+    }
+}
